408917985d0d04950eb4a725f15dbcb6d6ce1c1f
Arun Singhal <rit2015034@iiita.ac.in>
2017-03-24 18:29:45 +0530

    Add integration for paypal

--- helping_hands/settings.py
+++ helping_hands/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'community.apps.CommunityConfig',
+    'paypal.standard.ipn',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+PAYPAL_TEST = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
