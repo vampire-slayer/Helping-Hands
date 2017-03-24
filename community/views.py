3a9ca54a7851d1582b65f5ee601d85f8c256f30e
Arun Singhal <rit2015034@iiita.ac.in>
2017-03-24 12:29:08 +0530

    Add Template

--- community/views.py
+++ community/views.py
@@ -5,12 +5,14 @@ from . models import Community, Members
 # Create your views here.
 def index(request):
 	all_community = Community.objects.all()
-	html = ''
-	for community in all_community:
-		url = '/community/' + str(community.id) + '/'
-		html += '<a href ="' + url + '">' + community.name + '</a><br>'
+	# html = ''
+	# for community in all_community:
+	# 	url = '/community/' + str(community.id) + '/'
+	# 	html += '<a href ="' + url + '">' + community.name + '</a><br>'
 
-	return HttpResponse(html)
+	# return HttpResponse(html)
+	context = {'all_community' : all_community}
+	return render(request, 'community/index.html', context)
 
 def detail(request, community_id):
 	return HttpResponse("<h2> Details of Community " + str(community_id) + "</h2>")
